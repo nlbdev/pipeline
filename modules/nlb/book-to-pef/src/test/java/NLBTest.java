b1d7ba4b2ab3beda0c0a616185063873671d60a7
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2017-02-28 11:04:41 +0100

    Update to latest parents
    Also fix some book-to-pef tests:
    
    - xml:lang attribute on PEF was replaced with a dc:language meta
      element
    - Some tests started failing after the update but the expected
      results look suspicious. Needs to be verified.

--- modules/nlb/book-to-pef/src/test/java/NLBTest.java
+++ modules/nlb/book-to-pef/src/test/java/NLBTest.java
@@ -1,12 +1,7 @@
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.inject.Inject;
-
-import org.daisy.maven.xproc.xprocspec.XProcSpecRunner;
 
 import org.daisy.pipeline.braille.common.BrailleTranslator;
 import org.daisy.pipeline.braille.common.BrailleTranslatorProvider;
@@ -17,40 +12,17 @@ import org.daisy.pipeline.braille.common.TransformProvider;
 import static org.daisy.pipeline.braille.common.TransformProvider.util.dispatch;
 import static org.daisy.pipeline.braille.common.Query.util.query;
 
-import static org.daisy.pipeline.pax.exam.Options.brailleModule;
-import static org.daisy.pipeline.pax.exam.Options.calabashConfigFile;
-import static org.daisy.pipeline.pax.exam.Options.domTraversalPackage;
-import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
-import static org.daisy.pipeline.pax.exam.Options.logbackClassic;
-import static org.daisy.pipeline.pax.exam.Options.mavenBundle;
-import static org.daisy.pipeline.pax.exam.Options.mavenBundlesWithDependencies;
-import static org.daisy.pipeline.pax.exam.Options.logbackConfigFile;
-import static org.daisy.pipeline.pax.exam.Options.pipelineModule;
-import static org.daisy.pipeline.pax.exam.Options.thisBundle;
-import static org.daisy.pipeline.pax.exam.Options.xprocspec;
+import org.daisy.pipeline.junit.AbstractXSpecAndXProcSpecTest;
+
+import static org.daisy.pipeline.pax.exam.Options.thisPlatform;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.ops4j.pax.exam.Configuration;
-import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
-import org.ops4j.pax.exam.spi.reactors.PerClass;
-import org.ops4j.pax.exam.util.PathUtils;
-
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.systemPackage;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
-@RunWith(PaxExam.class)
-@ExamReactorStrategy(PerClass.class)
-public class NLBTest {
+public class NLBTest extends AbstractXSpecAndXProcSpecTest {
 	
 	@Inject
 	private BundleContext context;
@@ -110,70 +82,32 @@ public class NLBTest {
 		             fillLines(translator.lineBreakingFromStyledText().transform(styledText("busstopp","hyphens:auto")), 6));
 	}
 	
-	@Inject
-	private XProcSpecRunner xprocspecRunner;
-	
-	@Test
-	public void runXProcSpec() throws Exception {
-		File baseDir = new File(PathUtils.getBaseDir());
-		Map<String,File> tests = new HashMap<String,File>();
-		for (String test : new String[]{
-			"test_css_formatter",
-			"test_css_translator",
-			"test_dtbook-to-pef",
-			"test_dtbook-to-pef_hyphenation",
-			"test_epub3-to-pef",
-			"test_html-to-pef",
-			"test_script",
-			"test_translator"
-			})
-			tests.put(test, new File(baseDir, "src/test/xprocspec/" + test + ".xprocspec"));
-		boolean success = xprocspecRunner.run(tests,
-		                                      new File(baseDir, "target/xprocspec-reports"),
-		                                      new File(baseDir, "target/surefire-reports"),
-		                                      new File(baseDir, "target/xprocspec"),
-		                                      null,
-		                                      new XProcSpecRunner.Reporter.DefaultReporter());
-		assertTrue("XProcSpec tests should run with success", success);
-	}
-	
-	@Configuration
-	public Option[] config() {
-		return options(
-			logbackConfigFile(),
-			calabashConfigFile(),
-			domTraversalPackage(),
-			felixDeclarativeServices(),
-			thisBundle(),
-			junitBundles(),
-			systemPackage("javax.xml.stream;version=\"1.0.1\""),
-			mavenBundlesWithDependencies(
-				brailleModule("css-utils"),
-				brailleModule("pef-utils"),
-				brailleModule("liblouis-core"),
-				brailleModule("liblouis-utils"),
-				brailleModule("liblouis-tables"),
-				brailleModule("liblouis-native").forThisPlatform(),
-				brailleModule("libhyphen-core"),
-				brailleModule("libhyphen-libreoffice-tables"),
-				brailleModule("libhyphen-native").forThisPlatform(),
-				brailleModule("dotify-formatter"),
-				brailleModule("dtbook-to-pef"),
-				brailleModule("epub3-to-pef"),
-				brailleModule("html-to-pef"),
-				brailleModule("xml-to-pef"),
-				pipelineModule("file-utils"),
-				pipelineModule("common-utils"),
-				pipelineModule("html-utils"),
-				pipelineModule("zip-utils"),
-				pipelineModule("mediatype-utils"),
-				pipelineModule("file-utils"),
-				pipelineModule("fileset-utils"),
-				logbackClassic(),
-				xprocspec(),
-				mavenBundle("org.daisy.maven:xproc-engine-daisy-pipeline:?")
-			)
-		);
+	@Override
+	protected String[] testDependencies() {
+		return new String[] {
+			brailleModule("css-utils"),
+			brailleModule("pef-utils"),
+			brailleModule("liblouis-core"),
+			brailleModule("liblouis-utils"),
+			brailleModule("liblouis-tables"),
+			"org.daisy.pipeline.modules.braille:liblouis-native:jar:" + thisPlatform() + ":?",
+			brailleModule("libhyphen-core"),
+			brailleModule("libhyphen-libreoffice-tables"),
+			"org.daisy.pipeline.modules.braille:libhyphen-native:jar:" + thisPlatform() + ":?",
+			brailleModule("dotify-formatter"),
+			brailleModule("dtbook-to-pef"),
+			brailleModule("epub3-to-pef"),
+			brailleModule("html-to-pef"),
+			brailleModule("xml-to-pef"),
+			pipelineModule("file-utils"),
+			pipelineModule("common-utils"),
+			pipelineModule("html-utils"),
+			pipelineModule("zip-utils"),
+			pipelineModule("mediatype-utils"),
+			pipelineModule("file-utils"),
+			pipelineModule("fileset-utils"),
+			"org.daisy.pipeline:calabash-adapter:?",
+		};
 	}
 	
 	private Iterable<CSSStyledText> styledText(String... textAndStyle) {
