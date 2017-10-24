6682fa7b044e88495d0fbb5c789d5185e6600e87
Bert Frees <bertfrees@gmail.com>
2017-10-24 15:34:22 +0200

    [dotify] Increase the maximum number of volume splitting iterations some more

--- libs/dotify/dotify.formatter.impl/src/org/daisy/dotify/formatter/impl/FormatterImpl.java
+++ libs/dotify/dotify.formatter.impl/src/org/daisy/dotify/formatter/impl/FormatterImpl.java
@@ -132,7 +132,7 @@ public class FormatterImpl implements Formatter {
 
 		ArrayList<VolumeImpl> ret;
 
-		int maxIterations = 25;
+		int maxIterations = 50;
 		for (int j=1;j<=maxIterations;j++) {
 			try {
 				ret = new ArrayList<>();
