3ea2d353592bc4cc0c0ce83f49035cd7850c13a3
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2018-07-09 11:39:16 +0200

    fix .make/gradle-install.sh - cd to dirs relative to git project root

--- .make/gradle-install.sh
+++ .make/gradle-install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 [[ -n ${VERBOSE+x} ]] && set -x
 set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 for arg in "$@"; do
-    cd $arg
+    cd "$DIR/$arg"
     eval $GRADLE install
 done
